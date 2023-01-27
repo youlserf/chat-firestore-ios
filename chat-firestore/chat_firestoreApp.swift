3bd9d49d22adde4dc189abcc2848dbc913c4ff3f
youlserf <youlserf.cardenas@tecsup.edu.pe>
2023-01-27 09:42:19 -0800

    Initial Commit

---
+++ chat-firestore/chat_firestoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  chat_firestoreApp.swift
+//  chat-firestore
+//
+//  Created by youlserf on 27/01/23.
+//
+
+import SwiftUI
+
+@main
+struct chat_firestoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
